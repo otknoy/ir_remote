7139825cba15cff9697b284d7e2392963725beb1
otknoy <otknoy@gmail.com>
2016-06-27 03:19:56 +0900

    Add gulpfile

---
+++ ir_remote_fe/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+
+gulp.task('default', function () {
+    return gulp.src('index.html')
+	.pipe(vulcanize({
+	    abspath: '',
+	    excludes: [],
+	    stripExcludes: false,
+	    inlineScripts: true,
+	    inlineCss: true
+	}))
+	.pipe(gulp.dest('dest'));
+});
